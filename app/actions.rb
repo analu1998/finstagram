1401e7ef920d222a97f7e390637a5927a9ab4b0c
itw-van-01 <itw-van-01@lighthouselabs.com>
2019-06-27 18:53:59 -0400

    final

--- app/actions.rb
+++ app/actions.rb
@@ -1,103 +1,78 @@
 helpers do
   def current_user
-    User.find_by(id: session[:user_id])
-  end
-end
-
-def humanized_time_ago(minute_num)
-  if minute_num >= 60
-    "#{minute_num / 60} hours ago"
-  else
-    "#{minute_num} minutes ago"
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
 
 get '/' do
-  @finstagram_posts = FinstagramPost.order(created_at: :desc)
-  erb(:index)
-end
-
-get '/test' do
-  @user = {
-      "username": "Jonny123",
-      "friends": [
-        "tom",
-        "anna",
-        "will",
-        "Alex"
-        ]
-    }
-  erb(:test)
+  @finstagram_posts = FinstagramPost.order 'created_at DESC'
+  erb :index
 end
 
 get '/signup' do
-  @user= User.new
-  erb(:signup)
+  @user = User.new
+  erb :signup
 end
 
 post '/signup' do
-  email      = params[:email]
-  avatar_url = params[:avatar_url]
-  username   = params[:username]
-  password   = params[:password]
-
-  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
-
+  @user = User.new params.slice('email', 'avatar_url', 'username', 'password')
   if @user.save
-    redirect to('/login')
+    redirect to("/login")
   else
-    erb(:signup)
+    erb :signup
   end
 end
 
 get '/login' do
-  erb(:login)
+  erb :login
 end
 
 post '/login' do
-  username = params[:username]
-  password = params[:password]
-
-  user = User.find_by(username: username)  
-
-  if user && user.password == password
-    session[:user_id] = user.id
-    redirect to('/')
+  @user = User.find_by username: params[:username]
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect to("/")
   else
-    @error_message = "Login failed."
-    erb(:login)
+    @error_messages = ["Could not authenticate user."]
+    erb :login
   end
 end
 
 get '/logout' do
   session[:user_id] = nil
-  redirect to('/')
+  redirect to("/")
 end
 
+get '/posts/new' do
+  @post = FinstagramPost.new
+  erb :"posts/new"
+end
 
+get '/posts/:id' do
+  @post = FinstagramPost.find params[:id]
+  erb :"posts/show"
+end
 
-# before '/' do
-#   if (!current_user) 
-#      halt(401, 'Not authorized')
-#   end
-# #   redirect to('/login') unless current_user
-# end
+post '/posts' do
+  @post = FinstagramPost.new params.slice('photo_url', 'user_id')
+  if @post.save
+    redirect to("/")
+  else
+    erb :"posts/new"
+  end
+end
 
+post '/comments' do
+  Comment.create params.slice('text', 'finstagram_post_id', 'user_id')
+  redirect back
+end
 
+post '/likes' do
+  Like.create params.slice('finstagram_post_id', 'finstagram_post_id')
+  redirect back
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+delete '/likes/:id' do
+  Like.find(params[:id]).destroy
+  redirect back
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
+
 class User < ActiveRecord::Base
+
   has_many :finstagram_posts
   has_many :comments
   has_many :likes
+
+  validates_presence_of :email,
+                        :avatar_url,
+                        :username,
+                        :password
+
+  validates_uniqueness_of :email,
+                          :username
+
+  def like_for_finstagram_post_id(finstagram_post_id)
+    likes.find_by finstagram_post_id: finstagram_post_id
+  end
+
 end
